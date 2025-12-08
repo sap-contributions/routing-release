95edd7b402ff2d2fbb461503d6ce7472159dae81
Clemens Hoffmann <clemens.hoffmann@sap.com>
2025-12-08 10:40:34 +0100

    Update RouteSchema struct in route-emitter

--- src/code.cloudfoundry.org/route-registrar/config/config.go
+++ src/code.cloudfoundry.org/route-registrar/config/config.go
@@ -75,6 +75,8 @@ type RouteSchema struct {
 
 type Options struct {
 	LoadBalancingAlgorithm LoadBalancingAlgorithm `json:"loadbalancing,omitempty" yaml:"loadbalancing,omitempty"`
+	HashHeader             string                 `json:"hash_header,omitempty" yaml:"hash_header,omitempty"`
+	HashBalance            float64                `json:"hash_balance,omitempty" yaml:"hash_balance,omitempty"`
 }
 
 type LoadBalancingAlgorithm string
